a972c4d349ca295064f14f31cf874de74e708626
Morolari Titilope <tmorolari@gmail.com>
2018-10-31 13:39:02 +0100

    feature(UI): Setup end to end test for the frontend
    - Setup end to end test for react routes
    
    [Delivers #161617107]

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './home/ Home';
 import Login from './login/LoginForm';
+import SignUp from './signup';
 
 export default function Root() {
   return (
       <Switch>
         <Route exact={true} path='/' component={Home} />
         <Route exact path='/login' component={Login} />
+        <Route exact path='/register' component={SignUp} />
       </Switch>
   );
 }
